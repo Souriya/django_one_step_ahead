c54e769a654f3e4dc02bbd9d6352c1e3569168fd
Mong <mr.souriya@gmail.com>
2025-02-06 17:21:49 +0700

    updated custom backend auth file

--- django-project/apps/users/backends.py
+++ django-project/apps/users/backends.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth import authenticate as builtin_authenticate
+from django.contrib.auth import authenticate
 from .models import User
 
 class MultiAuthBackend(ModelBackend):
